71a65879732fabfa15d020165ed23a3c3f897d3f
Dave Preece <davep@iweb.co.uk>
2013-08-14 22:05:32 +0100

    Graph modes, add initial weight

---
+++ page/initial-weight-success.php
@@ -0,0 +1,6 @@
+<div class="alert success">
+	<div class="msg">
+		<h2>Thank You</h2>
+		<p>Your starting weight has been saved successfully.</p> <a href="<?php echo $userGraphPage;?>" class="button large blue">Go to your progress graph</a>
+	</div>
+</div>
---
+++ page/initial-weight.php
@@ -0,0 +1,63 @@
+<section class="reading-box main-accent clearfix">
+	<h2>Thank you for participating in Genesis' clinical trial</h2>
+	<p>Before we can get you up and running we just need to know what you're current weight is.</p>
+	<div class="tagline-shadow"></div>
+</section>
+
+<?php if(isset(GenesisTracker::$pageData['errors'])) : ?>
+	<div class="alert error spaced"><div class="msg">
+		<?php echo implode("<br />", GenesisTracker::$pageData['errors']);?>
+	</div></div>
+<?php
+endif;
+?>
+
+<form class="input-form user-tracking-input" action="" method="post" name="input-form">
+	<div class="question-container">
+		<div class="title">
+			<label for="weight_unit"><?php _e('Weight Units');?></label>
+			<div class="title-sep-container"><div class="title-sep"></div></div>
+		</div>
+		<p class="form-explanation"><?php _e('Whether you would like your weight to be saved as metric or imperial units');?></p>
+		<?php
+		echo $form->dropdown('weight_unit', array(
+				'1' => 'Stone and Pounds',
+				'2' => 'Kilograms'
+			), array(
+				'class' => 'weight-unit'
+			));
+		?> 
+		<button type="submit" name="action" value="changeunits" class="changeunits">Set</button>
+	</div>
+	
+	<div class="question-container">
+		<div class="title">
+			<label for="weight"><?php _e('Your Starting Weight');?></label>
+			<div class="title-sep-container"><div class="title-sep"></div></div>
+		</div>
+		<p class="form-explanation"><?php _e('Enter what your weight is at the start of the trial');?></p>
+		<div class="input-wrapper">
+			<?php
+			echo $form->input('weight_main', 'text', array(
+				'class' => 'general-input weight-input',
+				'id' => 'weight-main'
+				));
+			?>
+			<p class="input-suffix metric <?php echo (!$metricUnits ? 'hidden' : '');?>"><?php _e('kilograms');?></p>
+			<p class="input-suffix imperial <?php echo ($metricUnits ? 'hidden' : '');?>"><?php _e('stone');?></p>
+		</div>
+		<div class="input-wrapper">
+			<?php
+			echo $form->input('weight_pounds', 'text', array(
+				'class' => 'general-input weight-input imperial ' . ($metricUnits ? "hidden" : ""),
+				'id' => 'weight-pounds'
+				));
+			?>
+		
+			<p class="input-suffix imperial <?php echo ($metricUnits ? 'hidden' : '');?>"><?php _e('pounds');?></p>
+		</div>
+	</div>
+	<div class='button-c-container'>
+		<button type="submit" name="action" value="saveinitialweight" class="button large green saveform"><?php _e('Save your starting weight');?></button>
+	</div>
+</form>
