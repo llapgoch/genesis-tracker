96d53e96191145965405aa58d4ec678cbf03bc77
Dave Preece <dpreece@iweb.co.uk>
2014-10-14 21:58:41 +0100

    Version update

--- genesis-tracker.php
+++ genesis-tracker.php
@@ -3,7 +3,7 @@
 Plugin Name: Genesis Tracker
 Plugin URI: http://carbolowdrates.com
 Description: Tracks user's weight, calories, and exercise
-Version: 1.13
+Version: 1.14
 Author: Dave Preece
 Author URI: http://www.scumonline.co.uk
 License: GPL
