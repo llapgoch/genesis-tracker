fdcc3424f228c397eed7dfa03459d3179f7649f6
Dave Preece <dpreece@iweb.co.uk>
2014-10-22 17:16:08 +0100

    Wording changes, PDF links to open in new windows, index.php in plugin dirs

--- page/ineligible.php
+++ page/ineligible.php
@@ -13,7 +13,7 @@
                 </ul>        
             <p><?php _e("People who have previously been diagnosed with cancer or those with any of the health conditions listed on the previous page are unable to take part in the study as we are interested in the effect of diet and exercise in the <strong>prevention of cancer and other health conditions</strong>, that can be modified by diet and exercise. In addition, the use of certain medications may mean that you are unable to take part in the study as they can affect your risk of developing certain health problems. You can access further information or support services in relation to your current health or medications by <a href='" . $ineligibleDownloadPdfUrl . "' target='_blank'>downloading the supporting information document</a>");?></p>
             
-            <p><?php _e("If you are still interested in losing weight you can choose to follow our evidence-based 2 day diet by following the appropriate diet plan. The diet plans are part of a healthy lifestyle that includes exercise and they are designed to be:");?></p>
+            <p><?php _e("If you are overweight and are still interested in losing weight you can choose to follow our evidence-based 2 day diet by following the appropriate diet plan. The diet plans are part of a healthy lifestyle that includes exercise and they are designed to be:");?></p>
                 
             <ul>
                 <li>Low enough in calories to enable you to lose weight, but without leaving you feeling hungry.</li>
