72f9e7ab7cde0ed1dd34b130aa2ec07dbf2b4ee4
David Baker <dave@Davids-MacBook-Pro.local>
2016-12-20 21:39:37 +0000

    Increased version for installer

--- genesis-tracker.php
+++ genesis-tracker.php
@@ -3,7 +3,7 @@
 Plugin Name: Genesis Tracker
 Plugin URI: http://carbolowdrates.com
 Description: Tracks user's weight, calories, and exercise
-Version: 1.33
+Version: 1.34
 Author: Dave Baker
 Author URI: http://www.scumonline.co.uk
 License: GPL
