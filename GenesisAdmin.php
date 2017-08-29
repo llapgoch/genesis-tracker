e3886d0d96aa90edc78b129cb2a03b2978ea0688
Dave Baker <dave_preece@mac.com>
2017-08-29 21:37:06 +0100

    Stwich jQuery version to https

--- GenesisAdmin.php
+++ GenesisAdmin.php
@@ -17,7 +17,7 @@ class GenesisAdmin{
         global $pagenow;
         
         wp_enqueue_script('jquery-ui-datepicker');
-        wp_enqueue_style('jquery-style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css');
+        wp_enqueue_style('jquery-style', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css');
             
         // Use get as the function name to execute -- this is before the page rendering
         if(isset($_GET['sub']) && is_admin()){
