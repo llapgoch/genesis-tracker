2da981bdfe1aba6d5365501e2733a15167aef30e
Dave Baker <dave_preece@mac.com>
2017-11-19 19:49:32 +0000

    Added eligible bullet

--- page/eligibility-doctor.php
+++ page/eligibility-doctor.php
@@ -5,8 +5,9 @@
         </div>
         <div class="content">
             <ul>
-                <li>Because you answered yes to one or more of the exercise readiness questions, we would like you to seek further advice from your GP about your suitability for the study.</li>
-                <li>Please take this letter and the patient information sheet to your GP to complete before returning it to the dietitians. If you have any questions please contact the research dietitians on 0161 291 4413 or <a href="mailto:lifestyleresearch@nhs.net">lifestyleresearch@nhs.net</a>.</li>
+                <li>Because you answered yes to one or more of the exercise readiness questions, we would like you to seek further advice from your GP about your suitability for the study</li>
+                <li>Once you have downloaded the Doctor's letter, please highlight the question(s) which you answered yes to</li>
+                <li>Please take this letter and the patient information sheet to your GP to complete before returning it to the dietitians. If you have any questions please contact the research dietitians on 0161 291 4413 or <a href="mailto:lifestyleresearch@nhs.net">lifestyleresearch@nhs.net</a></li>
             </ul>
 
             <div class="button-c-container">
